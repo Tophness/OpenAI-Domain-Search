3fadaaa89bef83b6aafbd2d6d4760a66fe85e6eb
Chris Malone <chrismrulz@hotmail.com>
2023-06-01 20:49:11 +1000

    https

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 app.use(express.static("public"));
 
 app.use(
-  proxy("http://www.domain.com.au", {
+  proxy("https://www.domain.com.au", {
     proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
       if (srcReq.url.indexOf("/rent") !== -1) {
         proxyReqOpts.headers["content-type"] =
